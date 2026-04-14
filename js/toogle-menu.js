3f0f93f351c743f91a640479a82f34e069731e56
Firmanda Robby D. <02524147427@mail.mmtc.ac.id>
2026-04-14 16:45:09 +0700

    fix: mengatasi error tidak munculnya nav-links pada menu, menggunakan DOM

--- js/toogle-menu.js
+++ js/toogle-menu.js
@@ -1,41 +1,43 @@
 (function () {
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelector(".nav-links");
-const overlay = document.getElementById("overlay");
+    document.addEventListener("DOMContentLoaded", () => {
+        const hamburger = document.getElementById("hamburger");
+        const navLinks = document.querySelector(".nav-links");
+        const overlay = document.getElementById("overlay");
 
-function openMenu() {
-    navLinks.classList.add("active");
-    overlay.classList.add("active");
-    hamburger.classList.add("active");
-    document.body.classList.add("menu-open");
-}
+        function openMenu() {
+            navLinks.classList.add("active");
+            overlay.classList.add("active");
+            hamburger.classList.add("active");
+            document.body.classList.add("menu-open");
+        }
 
-function closeMenu() {
-    navLinks.classList.remove("active");
-    overlay.classList.remove("active");
-    hamburger.classList.remove("active");
-    document.body.classList.remove("menu-open");
-}
+        function closeMenu() {
+            navLinks.classList.remove("active");
+            overlay.classList.remove("active");
+            hamburger.classList.remove("active");
+            document.body.classList.remove("menu-open");
+        }
 
-/* toggle klik hamburger */
-hamburger.addEventListener("click", () => {
-    if (navLinks.classList.contains("active")) {
-        closeMenu();
-    } else {
-        openMenu();
-    }
-});
+        /* toggle klik hamburger */
+        hamburger.addEventListener("click", () => {
+            if (navLinks.classList.contains("active")) {
+                closeMenu();
+            } else {
+                openMenu();
+            }
+        });
 
-/* klik overlay = close */
-overlay.addEventListener("click", closeMenu);
+        /* klik overlay = close */
+        overlay.addEventListener("click", closeMenu);
 
-/* klik link = close */
-document.querySelectorAll(".navlinks").forEach(link => {
-    link.addEventListener("click", closeMenu);
-});
+        /* klik link = close */
+        document.querySelectorAll(".navlinks").forEach(link => {
+            link.addEventListener("click", closeMenu);
+        });
 
-/* tekan ESC = close */
-document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") closeMenu();
-});
+        /* tekan ESC = close */
+        document.addEventListener("keydown", (e) => {
+            if (e.key === "Escape") closeMenu();
+        });
+    });
 })();
